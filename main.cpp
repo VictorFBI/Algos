575218f5698104670581774d741ea618f4972e14
VictorFBI <124510561+VictorFBI@users.noreply.github.com>
2023-09-19 00:35:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,38 +4,45 @@
 #include <algorithm>
 
 void mergeSort(std::vector<int> &vec, int start, int end) {
-    if (end - start <= 1) return;
+  if (end - start <= 1) return;
 
-    mergeSort(vec, start, (end - start) / 2);
-    mergeSort(vec, (end - start) / 2, end);
-
-    std::vector<int> tmp;
-    int i = start, j = (end - start) / 2;
-
-    while ((i < (end - start) / 2) && j < end) {
-        if (vec[i] < vec[j]) {
-            tmp.push_back(vec[i]);
-            ++i;
-        } else {
-            tmp.push_back(vec[j]);
-            ++j;
-        }
+  if (end - start == 2) {
+    if (vec[start] > vec[start + 1]) {
+      std::swap(vec[start], vec[start + 1]);
+      return;
     }
+  }
 
-    if (i == (end - start) / 2) {
-        for (auto t = j; t < end; ++t) tmp.push_back(vec[t]);
+  mergeSort(vec, start, start + (end - start) / 2);
+  mergeSort(vec, start + (end - start) / 2, end);
+
+  std::vector<int> tmp;
+  int i = start, j = start + (end - start) / 2;
+
+  while (i < start + (end - start) / 2 && j < end) {
+    if (vec[i] < vec[j]) {
+      tmp.push_back(vec[i]);
+      ++i;
     } else {
-        for (auto t = i; t < (end - start) / 2; ++t) tmp.push_back(vec[t]);
+      tmp.push_back(vec[j]);
+      ++j;
     }
+  }
 
-    for (auto t = start; t < end; ++t) {
-        vec[t] = tmp[t - start];
-    }
+  if (i == start + (end - start) / 2) {
+    for (auto t = j; t < end; ++t) tmp.push_back(vec[t]);
+  } else {
+    for (auto t = i; t < start + (end - start) / 2; ++t) tmp.push_back(vec[t]);
+  }
+
+  for (auto t = start; t < end; ++t) {
+    vec[t] = tmp[t - start];
+  }
 }
 
 int main() {
-    std::vector<int> vec{5, 3};
-    mergeSort(vec, 0, vec.size());
-    std::for_each(vec.begin(), vec.end(), [](int &val) { std::cout << val << " "; });
-    return 0;
+  std::vector<int> vec{5, 3, 1, 4, -1, 90, 65, 77, -70, 11, -1, 256};
+  mergeSort(vec, 0, static_cast<int>(vec.size()));
+  std::for_each(vec.begin(), vec.end(), [](int &val) { std::cout << val << " "; });
+  return 0;
 }
