0e5bf5782b51da2ed0e211c2466d907591ae7a94
VictorFBI <124510561+VictorFBI@users.noreply.github.com>
2023-09-26 22:26:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <stack>
 
-template<typename T>
 class Queue {
  public:
   Queue() = default;
   ~Queue() = default;
 
-  Queue(const Queue &other) : pushStack(other.pushStack), popStack(other.popStack), _size(other._size) {
-  }
+  Queue(const Queue &other) = default;
 
   Queue(Queue &&other) noexcept
-      : pushStack(std::move(other.pushStack)), popStack(std::move(other.popStack)), _size(std::move(other._size)) {
+      : min(other.min),
+        _back(std::move(other._back)),
+        pushStack(std::move(other.pushStack)),
+        popStack(std::move(other.popStack)),
+        _size(other._size) {
   }
 
   Queue &operator=(const Queue &other) {
@@ -19,6 +21,8 @@ class Queue {
       pushStack = other.pushStack;
       popStack = other.popStack;
       _size = other._size;
+      min = other.min;
+      _back = other._back;
     }
 
     return *this;
@@ -29,142 +33,153 @@ class Queue {
       pushStack = std::move(other.pushStack);
       popStack = std::move(other.popStack);
       _size = other._size;
+      min = other.min;
+      _back = std::move(other._back);
       other._size = 0;
+      other.min = 0;
     }
 
     return *this;
   }
 
-  void push(T &val) {
-    pushStack.push(val);
-    if (increasing.empty() || val <= increasing.top()) increasing.push(val);
-    else {
-      while (!increasing.empty()) {
-        decreasing.push(increasing.top());
-        increasing.pop();
-      }
-
-      while (!decreasing.empty() && val < decreasing.top()) {
-        decreasing.pop();
-      }
-      decreasing.push(val);
-
-      while (!decreasing.empty()) {
-        increasing.push(decreasing.top());
-        decreasing.pop();
-      }
+  void push(int &val) {
+    if (pushStack.empty()) {
+      min = val;
+    } else {
+      if (val < min) min = val;
     }
+    _back = std::make_pair(val, min);
+    pushStack.emplace(val, min);
     ++_size;
   }
 
-  void push(T &&val) {
-    pushStack.push(val);
-    if (increasing.empty() || val <= increasing.top()) increasing.push(val);
-    else {
-      while (!increasing.empty()) {
-        decreasing.push(increasing.top());
-        increasing.pop();
-      }
-
-      while (!decreasing.empty() && val < decreasing.top()) {
-        decreasing.pop();
-      }
-      decreasing.push(val);
-
-      while (!decreasing.empty()) {
-        increasing.push(decreasing.top());
-        decreasing.pop();
-      }
+  void push(int &&val) {
+    if (pushStack.empty()) {
+      min = val;
+    } else {
+      if (val < min) min = val;
     }
+    _back = std::make_pair(val, min);
+    pushStack.emplace(val, min);
     ++_size;
   }
 
   void pop() {
     if (empty()) throw std::runtime_error("Queue is empty");
-
     if (popStack.empty()) {
-      int count = 0;
+      _back = pushStack.top();
+      int tmp_min;
       while (!pushStack.empty()) {
-        popStack.push(pushStack.top());
-        if (count == 0) _back = pushStack.top();
-        ++count;
+        if (popStack.empty()) tmp_min = pushStack.top().second;
+        else {
+          if (pushStack.top().second < tmp_min) tmp_min = pushStack.top().second;
+        }
+        popStack.emplace(pushStack.top().first, tmp_min);
         pushStack.pop();
       }
     }
-
-    if (increasing.top() == popStack.top()) {
-      increasing.pop();
+    if (popStack.top().first == popStack.top().second) {
+      popStack.pop();
+      std::stack<std::pair<int, int>> tmp;
+      int tmp_min;
+      while (!popStack.empty()) {
+        if (tmp.empty()) tmp_min = popStack.top().first;
+        else {
+          if (popStack.top().first < tmp_min) tmp_min = popStack.top().first;
+        }
+        tmp.emplace(popStack.top().first, tmp_min);
+        popStack.pop();
+      }
+      while (!tmp.empty()) {
+        popStack.emplace(tmp.top().first, tmp_min);
+        tmp.pop();
+      }
+    } else {
+      popStack.pop();
     }
-
-    popStack.pop();
     --_size;
   }
 
-  T &front() {
-    if (empty()) throw std::runtime_error("Stack is empty");
-
+  int &front() {
+    if (empty()) throw std::runtime_error("Queue is empty");
     if (popStack.empty()) {
-      int count = 0;
+      _back = pushStack.top();
+      int tmp_min;
       while (!pushStack.empty()) {
-        popStack.push(pushStack.top());
-        if (count == 0) _back = pushStack.top();
-        ++count;
+        if (popStack.empty()) tmp_min = pushStack.top().second;
+        else {
+          if (pushStack.top().second < tmp_min) tmp_min = pushStack.top().second;
+        }
+        popStack.emplace(pushStack.top().first, tmp_min);
         pushStack.pop();
       }
     }
-
-    return popStack.top();
+    return popStack.top().first;
   }
 
-  T &back() {
-    if (empty()) throw std::runtime_error("Stack is empty");
-
-    if (pushStack.empty()) return _back;
-    else return pushStack.top();
+  int &back() {
+    if (empty()) throw std::runtime_error("Queue is empty");
+    return _back.first;
   }
 
-  T &min() {
-    return increasing.top();
+  const int &getMin() {
+    if (empty()) throw std::runtime_error("Queue is empty");
+    if (!popStack.empty() && !pushStack.empty()) return std::min(pushStack.top().second, popStack.top().second);
+    if (!popStack.empty()) return popStack.top().second;
+    return pushStack.top().second;
   }
 
   void swap(Queue &other) {
     std::swap(*this, other);
   }
 
-  size_t size() {
+  [[nodiscard]] size_t size() const {
     return _size;
   }
 
-  bool empty() {
+  [[nodiscard]] bool empty() const {
     return _size == 0;
   }
 
  private:
-  T _back{};
-  std::stack<T> pushStack;
-  std::stack<T> popStack;
-  std::stack<T> increasing;
-  std::stack<T> decreasing;
+  int min{};
+  std::pair<int, int> _back{};
+  std::stack<std::pair<int, int>> pushStack;
+  std::stack<std::pair<int, int>> popStack;
   size_t _size = 0;
 };
 
 int main() {
-  Queue<int> q;
-  q.push(3);
-  q.push(4);
+  Queue q;
+  q.push(1);
+  q.push(-1);
+  q.push(0);
   q.push(5);
-  q.push(6);
-  q.push(7);
-  std::cout << q.min() << "\n";
+  q.push(3);
+  std::cout << "Front: " << q.front() << "\n";
+  std::cout << "Back: " << q.back() << " \n";
+  std::cout << "Min: " << q.getMin() << "\n";
+  std::cout << "\n";
   q.pop();
-  std::cout << q.min() << "\n";
+  std::cout << "Front: " << q.front() << "\n";
+  std::cout << "Back: " << q.back() << " \n";
+  std::cout << "Min: " << q.getMin() << "\n";
+  std::cout << "\n";
   q.pop();
-  std::cout << q.min() << "\n";
+  std::cout << "Front: " << q.front() << "\n";
+  std::cout << "Back: " << q.back() << " \n";
+  std::cout << "Min: " << q.getMin() << "\n";
+  std::cout << "\n";
   q.pop();
-  std::cout << q.min() << "\n";
+  std::cout << "Front: " << q.front() << "\n";
+  std::cout << "Back: " << q.back() << " \n";
+  std::cout << "Min: " << q.getMin() << "\n";
+  std::cout << "\n";
   q.pop();
-  std::cout << q.min() << "\n";
+  std::cout << "Front: " << q.front() << "\n";
+  std::cout << "Back: " << q.back() << " \n";
+  std::cout << "Min: " << q.getMin() << "\n";
+  std::cout << "\n";
   q.pop();
-
   return 0;
 }
