efeee134ed014b7ca21fd0b67e12e7d6e9b7f3e8
VictorFBI <124510561+VictorFBI@users.noreply.github.com>
2023-09-29 22:21:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,62 +1,201 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <algorithm>
-using namespace std;
+#include <unordered_map>
 
-int algorithm1(vector<int> a) {
-  int c = 0;
-  int ind = -1;
-  for (auto i = 0; i < a.size(); ++i) {
-    int c1 = 0;
-    for (int j = 0; j < a.size(); ++j) {
-      if (a[i] == a[j]) c1++;
-    }
-    if (c1 > c) {
-      c = c1;
-      ind = i;
-    }
+struct Node {
+  Node(int _val, Node *_prev, Node *_next) : val(_val), prev(_prev), next(_next) {
   }
-  if (c > a.size() / 2) return a[ind];
-}
+  int val;
+  Node *prev;
+  Node *next;
+};
 
-int algorithm2(vector<int> a) {
-  int c = 1;
-  int ind = 0;
-  for (auto i = 1; i < a.size(); ++i) {
-    if (a[ind] == a[i]) c++;
-    else c--;
-    if (c == 0) {
-      ind = i;
-      c = 1;
+class DoublyLinkedList {
+ public:
+  DoublyLinkedList() : size(0), head(nullptr), tail(nullptr) {
+  }
+  DoublyLinkedList(const DoublyLinkedList &other) : DoublyLinkedList() {
+    auto iter = other.head;
+    while (iter != nullptr) {
+      pushBack(iter->val);
+      iter = iter->prev;
+    }
+  }
+  explicit DoublyLinkedList(std::vector<int>& vec) : DoublyLinkedList() {
+    for (auto& el : vec) {
+      pushBack(el);
+    }
+  }
+  void pushBack(int el) {
+    if (size == 0) {
+      head = new Node(el, nullptr, nullptr);
+      tail = head;
+    } else {
+      tail->prev = new Node(el, nullptr, tail);
+      tail = tail->prev;
+    }
+    ++size;
+  }
+  void pushFront(int el) {
+    if (size == 0) {
+      head = new Node(el, nullptr, nullptr);
+      tail = head;
+    } else {
+      head->next = new Node(el, head, nullptr);
+      head = head->next;
+    }
+    ++size;
+  }
+  void insert(Node *prev, int data) {
+    if (prev == nullptr || !contains(prev)) {
+      throw std::runtime_error("Wrong position for insertion!");
+    }
+    if (prev->next == nullptr) {
+      pushFront(data);
+      return;
+    }
+    auto to_insert = new Node(data, prev, prev->next);
+    prev->next = to_insert;
+    prev->next->prev = to_insert;
+    ++size;
+  }
+  void popFront() {
+    if (size == 0) {
+      throw std::runtime_error("Deletion error!");
+    }
+    head = head->prev;
+    delete head->next;
+    head->next = nullptr;
+    --size;
+  }
+  void popBack() {
+    if (size == 0) {
+      throw std::runtime_error("Deletion error!");
+    }
+    tail = tail->next;
+    delete tail->prev;
+    tail->prev = nullptr;
+    --size;
+  }
+  void pop(Node *pos) {
+    if (pos == nullptr || !contains(pos)) {
+      throw std::runtime_error("Wrong position for deletion!");
+    }
+    if (pos->next == nullptr) {
+      popFront();
+      return;
+    }
+    if (pos->prev == nullptr) {
+      popBack();
+      return;
+    }
+
+    pos->next->prev = pos->prev;
+    pos->prev->next = pos->next;
+    delete pos;
+    --size;
+  }
+  void erase() {
+    if (size == 0) return;
+    while (size != 1) {
+      popBack();
+    }
+    delete head;
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+  }
+  void reverse() {
+    auto forward = head;
+    auto back = tail;
+    for (auto i = 1; i <= size / 2; ++i) {
+      std::swap(forward->val, back->val);
+      forward = forward->prev;
+      back = back->next;
+    }
+  }
+  void removeDuplicates() {
+    std::unordered_map<int, int> map;
+    auto iter = head;
+    auto tmp = new DoublyLinkedList();
+    while (iter != nullptr) {
+      if (map.count(iter->val) == 0) {
+        tmp->pushBack(iter->val);
+        map[iter->val] = 1;
+      }
+      iter = iter->prev;
+    }
+    erase();
+    head = tmp->head;
+    tail = tmp->tail;
+    size = tmp->size;
+  }
+  void replace(int el, int new_el) {
+    auto iter = head;
+    while (iter != nullptr) {
+      if (iter->val == el) iter->val = new_el;
+      iter = iter->prev;
     }
   }
 
-  int count = 0;
-  for(auto i = 0; i < a.size(); ++i) {
-    if (a[i] == a[ind]) ++count;
-    if (count > a.size() / 2) return a[ind];
-  }
-}
-int algorithm3(vector<int> a) {
-  if (a.size() == 1) return a[0];
-  auto c = 1;
-  std::sort(a.begin(), a.end());
-  auto i = 1;
-  for (; i < a.size(); ++i) {
-    if (a[i - 1] == a[i]) c++;
-    else {
-      if (c > a.size() / 2) return a[i - 1];
-      c = 1;
+  // delete this method in the final answer
+  void print() {
+    auto iter = head;
+    while (iter != nullptr) {
+      std::cout << iter->val << " ";
+      iter = iter->prev;
     }
+    std::cout << "\n";
   }
-  if (c > a.size() / 2) return a[i - 1];
-}
+
+ private:
+  std::size_t size;
+  Node *head;
+  Node *tail;
+  bool contains(Node *pos) {
+    auto iter = head;
+    while (iter != nullptr) {
+      if (iter == pos) return true;
+      iter = iter->prev;
+    }
+    return false;
+  }
+};
 
 int main() {
-  vector<int> a{1, 5, 3, 4, 5, 5, 5, 6, 5};
-  std::cout << algorithm1(a) << "\n";
-  std::cout << algorithm2(a) << "\n";
-  std::cout << algorithm3(a) << "\n";
+  std::vector<int> vec{1, 2, 3, 4, 5};
+  auto list = DoublyLinkedList(vec);
+  list.print();
+  list.popBack();
+  list.print();
+  list.popFront();
+  list.print();
+  list.pushBack(3);
+  list.pushBack(5);
+  list.pushBack(3);
+  list.pushBack(4);
+  list.pushBack(5);
+  list.print();
+  list.removeDuplicates();
+  list.print();
+  list.reverse();
+  list.print();
+  list.pushBack(1);
+  list.reverse();
+  list.print();
+  list.erase();
+  list.print();
+  list.pushBack(1);
+  list.pushFront(7);
+  list.print();
+  list.reverse();
+  list.print();
+  list.pushFront(7);
+  list.print();
+  list.removeDuplicates();
+  list.print();
+  list.pushBack(3);
+  list.reverse();
+  list.print();
   return 0;
 }
