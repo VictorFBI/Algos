7d097d2f19193f0d5bebfc244c44e46107e93c66
VictorFBI <124510561+VictorFBI@users.noreply.github.com>
2023-10-03 00:31:17 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 #include <string>
-#include <stack>
 
-std::string decode(std::string& str) {
+std::string Decode(std::string s) {
   std::string ans;
-  std::stack<int> digits;
-  std::stack<std::string> records;
-  for (auto &el : str) {
-    if (el == '[') continue;
-    if (el == ']' || isdigit(el)) {
-      if(!ans.empty()) records.push(ans);
-      ans.clear();
-      if (isdigit(el)) {
-        digits.push(el - 48);
+  for (std::size_t i = 0; i < s.size(); ++i) {
+    if (isdigit(s[i])) {
+      std::size_t num = 0;
+      while (isdigit(s[i])) {
+        num = num * 10 + (s[i] - '0');
+        ++i;
       }
-      continue;
+      std::string tmp;
+      std::size_t open = 1;
+      std::size_t close = 0;
+      std::size_t j = i + 1;
+      while (open != close) {
+        if (s[j] == '[') {
+          ++open;
+        }
+        if (s[j] == ']') {
+          ++close;
+        }
+        if (open == close) break;
+        tmp += s[j];
+        ++j;
+      }
+      auto pre_ans = Decode(tmp);
+      for (auto k = 0; k < num; ++k) {
+        ans += pre_ans;
+      }
+      i = j;
+    } else {
+      ans += s[i];
     }
-
-    ans += el;
   }
 
-  while(digits.size() != 1) {
-    auto size = digits.top();
-    std::string record;
-    for(auto i = 0; i < size; ++i) record += records.top();
-    digits.pop();
-    records.pop();
-    records.top() += record;
-  }
-
-  auto size = digits.top();
-  for(auto i = 0; i < size; ++i) ans += records.top();
   return ans;
 }
 
-
-
 int main() {
-  std::string str = "1[a2[bbb0[c]]]";
-  std::cout << decode(str);
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+
+  std::string input;
+  getline(std::cin, input);
+
+  std::cout << Decode(input);
   return 0;
 }
