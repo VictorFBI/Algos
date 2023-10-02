fa2e572c1878a288b715e22d1b926b886a3a82f2
VictorFBI <124510561+VictorFBI@users.noreply.github.com>
2023-10-02 13:49:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,16 +9,39 @@ int HammingDistance(std::vector<int> &a, std::vector<int> &b) {
   return count;
 }
 
+bool operator==(std::vector<int> &a, std::vector<int> &b) {
+  if (a.size() != b.size()) return false;
+
+  for (auto i = 0; i < a.size(); ++i) {
+    if (a[i] != b[i]) return false;
+  }
+
+  return true;
+}
+
+bool checkIn(std::vector<std::vector<int>> &set, std::vector<int> &vec) {
+  if (set.empty()) return false;
+  for (auto &el : set) {
+    if (el == vec) return true;
+  }
+
+  return false;
+
+}
+
 int CountArrayRotations(std::vector<int> &vec) {
   auto source = vec;
   auto n = vec.size();
   auto max_dist = 0;
   auto count = 0;
+  std::vector<std::vector<int>> set;
 
   for (auto i = 0; i < n; ++i) {
     for (auto j = 0; j < n - 1; ++j) {
       std::swap(vec[j], vec[j + 1]);
     }
+    if (checkIn(set, vec)) continue;
+    set.push_back(vec);
 
     auto dist = HammingDistance(source, vec);
     if (max_dist < dist) {
@@ -32,9 +55,15 @@ int CountArrayRotations(std::vector<int> &vec) {
 }
 
 int main() {
-  std::vector<int> vec1{1, 2, 3, 4};
-  std::vector<int> vec2{1, 1, 8, 0};
-  std::cout << CountArrayRotations(vec1) << std::endl;
-  std::cout << CountArrayRotations(vec2) << std::endl;
+  int n;
+  std::vector<int> vec;
+  std::cin >> n;
+
+  for (auto i = 0; i < n; ++i) {
+    int a;
+    std::cin >> a;
+    vec.push_back(a);
+  }
+  std::cout << CountArrayRotations(vec);
   return 0;
 }
